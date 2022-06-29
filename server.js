225985a15d8b625252e46fdd5552172c583648b0
leerivera <leerivera80@gmail.com>
2022-06-29 14:39:06 -0400

    first

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+
+const PORT = 8000
+
+
+const superheros = {
+    'spiderman': {
+        'birthName': 'Peter Parker',
+        'powers': 'super strenght, spidey-sense, wall-crawling'
+
+    },
+    'the punisher':{
+        'birthName': 'Frank Castle',
+        'powers': 'none, skilled combatant'
+    }
+
+}
+
+
+app.get('/', (request, response) =>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:superheroName', (request, response) => {
+     const superheroName = request.params.superheroName.toLocaleLowerCase()
+     if(superheros[superheroName]){
+        response.json(superheros[superheroName])
+     }else{
+        response.json("not a hero")
+     }
+})
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`the server is running on ${PORT}! damn that's fast`)
+})
