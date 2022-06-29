76ab493d19921a7fa9788a3025ea7d1a44a70965
leerivera <leerivera80@gmail.com>
2022-06-29 15:45:46 -0400

    puts data on DOM

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert("workingS")
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const superheroName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://simple-super-hero-api.herokuapp.com/api/${superheroName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.powers
+    } catch(error){
+        console.log(error)
+    }
+}
