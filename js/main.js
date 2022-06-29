6ee9aa98b9fe58c2905b71ab8385c148e34fc3f5
leerivera <leerivera80@gmail.com>
2022-06-29 16:02:02 -0400

    more

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const superheroName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://simple-super-hero-api.herokuapp.com/api/:${superheroName}`)
+        const response = await fetch(`https://simple-super-hero-api.herokuapp.com/api/${superheroName}`)
         const data = await response.json()
 
         console.log(data)
